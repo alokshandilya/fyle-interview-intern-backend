00ba8cd0b694450916e8c65a0fe0025251a25728
Alok Shandilya <alok.shandilya10@gmail.com>
2024-08-04 21:20:01 +0530

    add `/principals/teachers` API endpoint
    - not in principals_test.py but given in instructions

--- core/apis/assignments/principal.py
+++ core/apis/assignments/principal.py
@@ -5,7 +5,7 @@ from core.apis.assignments.student import submit_assignment
 from core.apis.responses import APIResponse
 from core.models.assignments import Assignment
 
-from .schema import AssignmentSchema
+from .schema import AssignmentSchema, AssignmentGradeSchema, TeacherSchema
 principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
 
 
@@ -16,4 +16,13 @@ def list_principal_assignments(p):
     submitted_and_graded_assignments = Assignment.get_submitted_and_graded_assignments()
     submitted_and_graded_assignments_dump = AssignmentSchema().dump(submitted_and_graded_assignments, many=True)
     return APIResponse.respond(data=submitted_and_graded_assignments_dump)
+
+
+@principal_assignments_resources.route('/teachers', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def list_teachers(p):
+    """Returns list of all teachers"""
+    all_teachers = Teacher.get_all_teachers()
+    all_teachers_dump = TeacherSchema().dump(all_teachers, many=True)
+    return APIResponse.respond(data=all_teachers_dump)
 
